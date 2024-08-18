138bc3df1581b3d4badede746523f235d9901156
varshaadake <varshaadake3014@gmail.com>
2024-08-18 15:49:11 +0530

    [varsha] new branch created and updated COV_TC_123_AgeValidation

---
+++ eclipse-workspace/CoverFoxTest/src/test/java/coverFoxTest/COV_TC_123_AgeValidation.java
@@ -0,0 +1,11 @@
+package coverFoxTest;
+
+import org.testng.annotations.Test;
+
+public class COV_TC_123_AgeValidation {
+  @Test
+  public void COV_TC_123_AgeValidation() 
+  {
+	  System.out.println("Age is valid");
+  }
+}
